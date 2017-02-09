05af9351cc909c8fb69e0bda48a2816417a594a3
juntarooo <sapamysp@gmail.com>
2017-02-08 22:45:30 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,10 +2,12 @@
 //slider to change k
 var d = 8;
 var n = 5;
-var sliderD;
-var sliderN;
-var sliderR;
-var sliderB;
+
+// var block;
+// var sliderD;
+// var sliderN;
+// var sliderR;
+// var sliderB;
 
 //color
 var col = {
@@ -16,20 +18,26 @@ var col = {
 };
 function setup() {
   canvas = createCanvas(600, 600);
+  frameRate(6);
+  textSize(20);
+  textSize(30);
+  textAlign(CENTER);
   // // colorMode(HSB);
   // sliderD = createSlider(0, 360, 60, 40);
   // sliderD.position(10, 10);
   // sliderD.style('width', '80px');
-  sliderD = createSlider(1, 10, 5, 0.1);
-  sliderN = createSlider(1, 10, 5, 0.1);
-  sliderR = createSlider(100, 275, 1, 1);
-  sliderB = createSlider(100, 175, 1, 1);
+  //
+  // sliderD = createSlider(1, 10, 5, 0.1);
+  // sliderN = createSlider(1, 10, 5, 0.1);
+  // sliderR = createSlider(100, 275, 1, 1);
+  // sliderB = createSlider(100, 175, 1, 1);
 
 }
 
 function draw() {
-    d = sliderD.value();
-    n = sliderN.value();
+    text(frameCount, 580, 580);
+    d = ceil(random(1,10));
+    n = ceil(random(1,10));
     var k = n / d;
     background(51);
     translate(width / 2, height /2); // put drawing in center.
@@ -41,9 +49,9 @@ function draw() {
       // stroke(255);
       // strokeWeight(3);
       noStroke();
-      col.r = sliderR.value();
+      col.r = random(100,255);
       col.g = 0;
-      col.b = sliderB.value();
+      col.b = random(100, 175);
       fill(col.r, col.g, col.b, 200);
       ellipse(x,y, 5, 5);
       noFill();
